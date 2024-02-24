7b86f75cfa51685d4813fc7eeb849438e55df0a1
HristiqnK <155568238+HristiqnK@users.noreply.github.com>
2024-02-25 00:17:56 +0200

    Delete FitNavigator directory

--- FitNavigator/quizLogic/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class QuizlogicConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'quizLogic'
--- FitNavigator/quizLogic/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from . import views
-
-urlpatterns = [
-    path('fitNavigator', views.quiz, name="fitNavigator"),
-    path('bmi', views.bmi, name="bmi"),
-    path('results', views.results, name="results")
-]
